2ad931ac8f4dd3c582a391ffa866ad89da51024f
pedromoura <pedromamoura@hotmail.com>
2016-07-13 16:17:00 +0100

    Added request dependency, started implementation.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var wolfram = require('wolfram');
 
+var testController = require('/controller/testCtrl.js');
 
 var app = express();
-app.use(bodyparser.json());
+app.use(bodyparser.text());
+
 var port = process.env.PORT || 5000;
 var router = express.Router();
 
 
-router.get('/', function(req, res) {
-	res.send("The window starts here");
+router.get('/start', function (req, res) {
+	testController.start(req, res);
+	res.sendStatus(200);
 });
 
 app.use('/api', router);
 
 app.listen(port);
-console.log('app live in port: ' + port);
+console.log('app live in port: ' + port + '\nGET /api/start?name=pedro to kick things off');
