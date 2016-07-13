96d28c465fde10cb7fb9b0fee3e9e4f215b11f65
pedromoura <pedromamoura@hotmail.com>
2016-07-13 18:26:52 +0100

    Added model

---
+++ model/testmodel.js
@@ -0,0 +1,14 @@
+var _ = require('lodash');
+var baseUrl = '';
+
+module.exports = {
+	setName: function(name){
+		var name = req.query.name;
+
+		if(!_.isNil(name)) {
+			
+		}else{
+			console.log("name missing.")
+		}
+	}
+};
